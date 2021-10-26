7e4749a4c0a3f466353c158885a92b7cd8ed6782
Agustin <agustindariodelgado@gmail.com>
2021-10-25 21:54:12 -0300

    Fixed dont show product image

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,13 +2,12 @@ import Item from "./Item"
 
 function ItemList (items){
 
-
     return (
         <>
             {
                 items.productos.map(prod => 
 
-                    <Item key={prod.identificador} nombre={prod.nombre} precio={prod.precio} descripcion={prod.descripcion}/>
+                    <Item id={prod.id} nombre={prod.nombre} precio={prod.precio} descripcion={prod.descripcion} img={prod.img}/> 
                 )
             }
         </>
